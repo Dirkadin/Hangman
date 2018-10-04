1c408e82df1d77a22499f8d48878de474fab9fea
dirk <dirkadin@gmail.com>
2018-10-04 15:48:08 -0500

    Initial Commit

---
+++ Hangman2/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  Hangman2
+//
+//  Created by Dirk on 10/4/18.
+//  Copyright © 2018 Dirk. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+	// insert code here...
+	printf("Hello, World!\n");
+	return 0;
+}
